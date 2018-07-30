677db6c0d5e616aba52107de1b2b586144652ba4
hjalmar <hjalmar.basile@icloud.com>
2018-07-30 17:43:37 +0200

    Inflate detail activity menu

--- app/src/main/java/com/hjalmar/android/inventoryapp/DetailActivity.java
+++ app/src/main/java/com/hjalmar/android/inventoryapp/DetailActivity.java
@@ -2,6 +2,8 @@ package com.hjalmar.android.inventoryapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -10,4 +12,25 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_save:
+                break;
+            case R.id.action_delete:
+                break;
+            default:
+                throw new IllegalArgumentException("Unexpected action id: " + item.getItemId());
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
